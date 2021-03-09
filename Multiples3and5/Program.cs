e5924e622d40551b965b72924af6fa538385e064
bslenker <slenkerbenjamin@gmail.com>
2021-03-08 19:27:49 -0600

    First attempt.

--- Multiples3and5/Program.cs
+++ Multiples3and5/Program.cs
@@ -10,6 +10,20 @@ namespace Multiples3and5
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            //Find the sum of all the multiples of 3 or 5 below 1000 and print it out to the console.
+
+
+            List<int> mult = new List<int>();
+            for (int i = 3; i < 1000; i++)
+            {
+                mult.Add(i);
+            }
+            for (int x = 5; x < 1000; x++)
+            {
+                mult.Add(x);
+            }
+            Console.WriteLine(mult.Sum());
+
+        }//end Main()
+    }//end class
+}//end namespace
